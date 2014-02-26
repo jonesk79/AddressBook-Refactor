d991efa1386edc0115ae5137675f7c031311e95a
Peter Tinti & KeMonté Jones <student@epicodus.com>
2014-02-26 09:17:51 -0800

    just getting ready for wednsday

---
+++ address book/spec/specs.js
@@ -0,0 +1,15 @@
+describe("Contact", function() {
+  describe("createAddress", function() {
+    it("creates an address object", function() {
+      var testContact = Contact.create();
+      var testAddress = testContact.createAddress();
+      Address.isPrototypeOf(testAddress).should.equal(true);
+    });
+
+    it("adds the address to the addresses property of the contact", function() {
+      var testContact = Contact.create();
+      var testAddress = testContact.createAddress();
+      testContact.addresses.should.eql([testAddress]);
+    });
+  });
+});
